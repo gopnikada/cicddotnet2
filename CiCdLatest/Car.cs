4ad4f9fe1432703da73669751db0bd805f864747
gopnikada <33294787+gopnikada@users.noreply.github.com>
2023-11-06 17:16:30 +0100

    Add project files.

---
+++ CiCdLatest/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CiCdLatest
+{
+    public class Car
+    {
+        public int _year { get; set; }
+        public Car(int year)
+        {
+            _year = year;
+        }
+        
+        public string GetYear()
+        {
+            switch (_year)
+            {
+                case int n when (n <= 2000):
+                    return "old";
+                case int n when (n > 2000 && n<=2015):
+                    return "modern";
+                case int n when (n >= 2015):
+                    return "new";
+                default:
+                    return "";
+
+            }
+        }
+    }
+}
---
+++ CiCdLatestTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+using CiCdLatest;
+using Xunit;
+
+namespace CiCdLatestTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var oldCar = new Car(1995);
+            var modernCar = new Car(2005);
+            var newCar = new Car(2020);
+            //act
+            var resultOldCar = oldCar.GetYear();
+            var resultModernCar = modernCar.GetYear();
+            var resultNewCar = newCar.GetYear();
+            //assert
+            //resultOldCar.Should().Be("old");
+            //resultModernCar.Should().Be("modern");
+            //resultNewCar.Should().Be("new");
+
+            Assert.Equal("old", resultOldCar);
+        }
+    }
+}
